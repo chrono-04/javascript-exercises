69a46cf978035a679bbdfc1d32263ef97dc6e918
chrono-04 <danielpenaflor04@gmail.com>
2025-12-12 09:53:47 +0800

    completed 07_reverseString exercise

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  return string.split("").reverse().join("");
 };
+
+console.log(reverseString("chrono"));
+console.log(reverseString("hello there"));
+console.log(reverseString("123! abc! Hello, Odinite."));
+console.log(reverseString(""));
 
 // Do not edit below this line
 module.exports = reverseString;
