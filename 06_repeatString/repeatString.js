477f9877b82fddd41d8cdace5d5b98571385fae0
chrono-04 <danielpenaflor04@gmail.com>
2025-12-12 08:58:40 +0800

    completed 06_repeatString exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,26 @@
-const repeatString = function() {
-
+const repeatString = function (string, amount) {
+  let finalString = "";
+  if (amount < 0) {
+    finalString = "ERROR";
+  }
+  for (let i = 0; i < amount; i++) {
+    finalString = finalString += string;
+  }
+  return finalString;
 };
+
+function randomNum(num) {
+  num = Math.floor(Math.random() * num);
+  return num;
+}
+
+console.log(repeatString("hey", 3));
+console.log(repeatString("hello", 10));
+console.log(repeatString("hi", 1));
+console.log(repeatString("bye", 0));
+console.log(repeatString("goodbye", -1));
+console.log(repeatString("hey", randomNum(10)));
+console.log(repeatString("", 10));
 
 // Do not edit below this line
 module.exports = repeatString;
