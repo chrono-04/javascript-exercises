d32dda21d6d343e3f109d371a05eb11c4a71993d
chrono-04 <danielpenaflor04@gmail.com>
2025-12-12 17:22:23 +0800

    completed 10_leapYears exercise

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
 };
+
+console.log(leapYears(1996));
+console.log(leapYears(1997));
+console.log(leapYears(34992));
+console.log(leapYears(1900));
+console.log(leapYears(1600));
+console.log(leapYears(700));
 
 // Do not edit below this line
 module.exports = leapYears;
