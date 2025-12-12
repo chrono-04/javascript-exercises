2ae6b3c19f8a8d3b4c6ce36ad94ef3d45db0c6a9
chrono-04 <danielpenaflor04@gmail.com>
2025-12-12 16:08:41 +0800

    completed 08_removeFromArray exercise

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...args) {
+  array = array.filter((item) => !args.includes(item));
+  return array;
 };
+
+console.log(removeFromArray([1, 2, 3, 4], 3));
+console.log(removeFromArray([1, 2, 3, 4], 2, 3));
+console.log(removeFromArray([1, 2, 2, 4], 2));
+console.log(removeFromArray([1, 2, 3, 4], 7, "tacos"));
+console.log(removeFromArray([1, 2, 3, 4], 7, 2));
+console.log(removeFromArray([1, 2, 3, 4], 1, 2, 3, 4));
+console.log(removeFromArray(["hey", 2, 3, "ho"], "hey", 3));
+console.log(removeFromArray([1, 2, 3, 4], "1", 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
