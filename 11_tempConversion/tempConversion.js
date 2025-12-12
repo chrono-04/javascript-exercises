fedc4d4dd7e45b09cda32270d6b0966317b15e52
chrono-04 <danielpenaflor04@gmail.com>
2025-12-12 17:39:31 +0800

    completed 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,31 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temp) {
+  let result = ((temp - 32) * 5) / 9;
+  if (result === 0) {
+    return Math.round(result);
+  } else {
+    return Number(result.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temp) {
+  let result = (temp * 9) / 5 + 32;
+  if (result === 0) {
+    return Math.round(result);
+  } else {
+    return Number(result.toFixed(1));
+  }
 };
+
+console.log(convertToCelsius(32));
+console.log(convertToCelsius(100));
+console.log(convertToCelsius(-100));
+
+console.log(convertToFahrenheit(0));
+console.log(convertToFahrenheit(73.2));
+console.log(convertToFahrenheit(-10));
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
