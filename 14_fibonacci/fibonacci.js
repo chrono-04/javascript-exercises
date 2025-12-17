69f673242785bec8ab9f9322f13094e94f1b4323
chrono-04 <danielpenaflor04@gmail.com>
2025-12-18 00:34:40 +0800

    completed 14_fibonacci exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  if (n < 0) {
+    return "OOPS";
+  }
+  let fibSequence = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    fibSequence[i] = fibSequence[i - 1] + fibSequence[i - 2];
+  }
+  return fibSequence[n];
 };
+
+console.log(fibonacci(4));
+console.log(fibonacci(6));
+console.log(fibonacci(10));
+console.log(fibonacci(15));
+console.log(fibonacci(25));
+console.log(fibonacci(0));
+console.log(fibonacci(-25));
+console.log(fibonacci("0"));
+console.log(fibonacci("1"));
+console.log(fibonacci("2"));
+console.log(fibonacci("8"));
 
 // Do not edit below this line
 module.exports = fibonacci;
