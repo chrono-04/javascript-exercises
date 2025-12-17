a5c7f13ce0197063ff5638bc5ae16ae386a14694
chrono-04 <danielpenaflor04@gmail.com>
2025-12-17 15:11:24 +0800

    completed 12_calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,26 +1,55 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arr = []) {
+  return arr.reduce((acc, currentVal) => {
+    return acc + currentVal;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr = []) {
+  return arr.reduce((acc, currVal) => {
+    return acc * currVal;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function (num, pow) {
+  return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n < 0) return NaN;
+  let result = 1;
+  for (let i = 2; i <= n; i++) result *= i;
+  {
+    return result;
+  }
 };
+
+console.log(add(0, 0));
+console.log(add(2, 2));
+console.log(add(2, 6));
+console.log(subtract(10, 4));
+console.log(subtract(-10, -4));
+console.log(subtract(-8, 7));
+console.log(sum([]));
+console.log(sum([7]));
+console.log(sum([7, 11]));
+console.log(sum([1, 3, 5, 7, 9]));
+console.log(multiply([2, 4]));
+console.log(multiply([2, 4, 6, 8, 10, 12, 14]));
+console.log(power(4, 3));
+console.log(power(3, 10));
+console.log(factorial(5));
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(5));
+console.log(factorial(10));
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +58,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
