13182eddaf035f9bfffefe5b474b7b618f15a305
chrono-04 <danielpenaflor04@gmail.com>
2025-12-17 15:56:22 +0800

    completed 13_palindromes exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const cleaned = word.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  const reversedWord = cleaned.split("").reverse().join("");
+  return reversedWord === cleaned;
 };
+
+console.log(palindromes("racecar"));
+console.log(palindromes("racecar!"));
+console.log(palindromes("Racecar!"));
+console.log("A car, a man, a maraca.");
+console.log("Animal loots foliated detail of stool lamina.");
+console.log("ZZZZ car, a man, a maracaz.");
+console.log("rac3e3car");
+console.log("r3ace3car");
 
 // Do not edit below this line
 module.exports = palindromes;
