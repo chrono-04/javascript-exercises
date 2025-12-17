c5218ec42157fb50ff67c9ffb0cecc3173ca81a1
chrono-04 <danielpenaflor04@gmail.com>
2025-12-18 00:44:35 +0800

    completed 15_getTheTitles exercise

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,6 +1,22 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+const getTheTitles = function (prop) {
+  const titles = prop.map((item) => {
+    return item.title;
+  });
+  return titles;
 };
+
+console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
