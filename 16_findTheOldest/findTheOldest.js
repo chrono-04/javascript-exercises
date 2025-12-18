bad53b63f588e966568ea9bbaf7fe27e70ea3e5e
chrono-04 <danielpenaflor04@gmail.com>
2025-12-18 10:45:46 +0800

    completed 16_findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,75 @@
-const findTheOldest = function() {
+const people = [
+  {
+    name: "Carly",
+    yearOfBirth: 1942,
+    yearOfDeath: 1970,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
 
+const people2 = [
+  {
+    name: "Carly",
+    yearOfBirth: 2018,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+
+const people3 = [
+  {
+    name: "Carly",
+    yearOfBirth: 1066,
+  },
+  {
+    name: "Ray",
+    yearOfBirth: 1962,
+    yearOfDeath: 2011,
+  },
+  {
+    name: "Jane",
+    yearOfBirth: 1912,
+    yearOfDeath: 1941,
+  },
+];
+const getAge = function (birth, death) {
+  if (!death) {
+    death = new Date().getFullYear();
+  }
+  return death - birth;
 };
+
+const findTheOldest = function (arr) {
+  return arr.reduce((oldest, currentPerson) => {
+    const oldestPersonAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentPersonAge = getAge(
+      currentPerson.yearOfBirth,
+      currentPerson.yearOfDeath,
+    );
+    return oldestPersonAge < currentPersonAge ? currentPerson : oldest;
+  });
+};
+
+console.log(findTheOldest(people).name);
+console.log(findTheOldest(people2).name);
+console.log(findTheOldest(people3).name);
 
 // Do not edit below this line
 module.exports = findTheOldest;
